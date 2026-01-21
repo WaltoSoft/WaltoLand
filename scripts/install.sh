5a7db5822c93ef9d417f626c636ad9d1e39f8e33
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2026-01-21 11:03:18 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ executeScript() {
   source $SCRIPT_DIR/packageInstall.sh
   source $SCRIPT_DIR/dotfiles.sh
   source $SCRIPT_DIR/bashrc.sh
-#  source $SCRIPT_DIR/services.sh
+  source $SCRIPT_DIR/services.sh
 #  source $SCRIPT_DIR/grub.sh
 #  source $SCRIPT_DIR/gnome.sh
 #  source $SCRIPT_DIR/reboot.sh
