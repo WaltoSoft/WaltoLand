45307ca8a35a582bfa698c9a5b6820e443c07a6d
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2026-01-21 13:04:18 -0600

    Add script to configure Grub settings and themes

---
+++ scripts/grub.sh
@@ -0,0 +1,34 @@
+executeScript() {
+  local grubScriptFile=/boot/grub/grub.cfg
+  local grubConfigFile=/etc/default/grub
+  local grubThemesFolder=/usr/share/grub/themes
+
+  if $(isPackageInstalled grub) && [ -f $grubScriptFile ] ; then
+    if [ ! -f "${grubConfigFile}.waltoland.bkp" ] && [ ! -f "${grubScriptFile}.waltoland.bkp" ]; then
+      echoText -f "Grub"
+      echoText "Configuring Grub"
+
+      cp $grubConfigFile "${grubConfigFile}.waltoland.bkp"
+      cp $grubScriptFile "${grubScriptFile}.waltoland.bkp"
+
+      ensureFolder $grubThemesFolder/arch-linux
+
+      echoText "Setting grub theme to arch-linux"
+      tar -C $grubThemesFolder/arch-linux -xf $REPO_DIR/archives/grub/arch-linux.tar
+      
+      sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
+      /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1920x1080
+      /^GRUB_TERMINAL_OUTPUT=console/c\#GRUB_TERMINAL_OUTPUT=console
+      /^GRUB_THEME=/c\GRUB_THEME=\"${grubThemesFolder}/arch-linux/theme.txt\"
+      /^#GRUB_THEME=/c\GRUB_THEME=\"${grubThemesFolder}/arch-linux/theme.txt\"
+      /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" $grubConfigFile
+
+      echoText "Making the new grub script file"
+      grub-mkconfig -o $grubScriptFile
+
+      echoText -c $COLOR_SUCCESS "Grub configuration complete!"
+    fi
+  fi
+}
+
+executeScript
