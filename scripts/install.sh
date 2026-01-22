bc21dac4efbd65b181fb2e23ccfb4cabb60c0b1e
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2026-01-21 19:52:16 -0600

    Install WaltoLand desktop file for Wayland session
    Add installation of WaltoLand desktop file and source reboot script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,11 @@ executeScript() {
   source $SCRIPT_DIR/bashrc.sh
   source $SCRIPT_DIR/services.sh
   source $SCRIPT_DIR/grub.sh
-#  source $SCRIPT_DIR/gnome.sh
-  source $SCRIPT_DIR/reboot.sh
 
+  echoText "Installing WaltoLand desktop file as an option for launching a wayland session
+  sudo cp $SCRIPT_DIR/waltoland.desktop /usr/share/wayland-sessions/waltoland.desktop
+
+  source $SCRIPT_DIR/reboot.sh
   echoText -c $COLOR_SUCCESS "Installation complete!"
 }
 
