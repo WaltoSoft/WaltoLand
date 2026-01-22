cc13ad49f86904582763632092662a3cde76b9d1
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2026-01-21 20:15:01 -0600

    Fix echoText command for installation message

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ executeScript() {
   source $SCRIPT_DIR/services.sh
   source $SCRIPT_DIR/grub.sh
 
-  echoText "Installing WaltoLand desktop file as an option for launching a wayland session
+  echoText "Installing WaltoLand desktop file as an option for launching a wayland session"
   sudo cp $SCRIPT_DIR/waltoland.desktop /usr/share/wayland-sessions/waltoland.desktop
 
   source $SCRIPT_DIR/reboot.sh
